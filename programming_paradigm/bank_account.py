ad944b000a06b63f2b8242b0f6a988e75bada77f
Jeffrey Asamoah <123427419+JeffreyAsiedu@users.noreply.github.com>
2025-11-30 23:11:50 +0000

    Update bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,6 +1,6 @@
 class BankAccount:
-    def __init__(self, initial_balance=0.0):
-        self.__account_balance = initial_balance   # Encapsulated attribute
+    def display_balance(self):
+    print(f"Current Balance: ${self.__account_balance:.2f}")
 
     def deposit(self, amount):
         if amount > 0:
